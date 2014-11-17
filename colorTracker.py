b0e067bc3ee078fa6ea72d18a48125addef2f1b6
8HoSsEiN8 <h.ghaffarinik@gmail.com>
2014-11-17 01:23:24 -0500

    working color/face tracker that outputs the error in x & y with respect to the center of the image. myController provides appropriate theta in radians and dynController should be able to control the servos.  I think these should work, but we need to verify it as I don't have the servos.  We may just need to adjust for +/- in the controller.

--- colorTracker.py
+++ colorTracker.py
@@ -1,31 +1,4 @@
 #!/usr/bin/env python
-# /* -*-  indent-tabs-mode:t; tab-width: 8; c-basic-offset: 8  -*- */
-# /*
-# Copyright (c) 2014, Daniel M. Lofaro <dan (at) danLofaro (dot) com>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the author nor the names of its contributors may
-#       be used to endorse or promote products derived from this software
-#       without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# */
 
 import controller_include as ci
 import csv
@@ -43,113 +16,70 @@ import numpy as np
 
 import actuator_sim as ser
 
-dd = diff_drive
-#ref = dd.H_REF()
-tim = dd.H_TIME()
-
-#ROBOT_DIFF_DRIVE_CHAN   = 'robot-diff-drive'
-#ROBOT_CHAN_VIEW   = 'robot-vid-chan'
-#ROBOT_TIME_CHAN  = 'robot-time'
 
 CONTROLLER_REF_NAME  = 'controller-ref-chan'
-
-# CV setup 
-cv.NamedWindow("wctrl", cv.CV_WINDOW_AUTOSIZE)
-#capture = cv.CaptureFromCAM(0)
-#capture = cv2.VideoCapture(0)
-
-# added
-##sock.connect((MCAST_GRP, MCAST_PORT))
-newx = 320
-newy = 240
-
-nx = 640
-ny = 480
-
-#r = ach.Channel(ROBOT_DIFF_DRIVE_CHAN)
-#r.flush()
-#v = ach.Channel(ROBOT_CHAN_VIEW)
-#v.flush()
-#t = ach.Channel(ROBOT_TIME_CHAN)
-#t.flush()
-
-
 e = ach.Channel(ci.CONTROLLER_REF_NAME)
 e.flush()
 controller = ci.CONTROLLER_REF()
 
-i=0
-Ts = .1
 
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+height, width, depth = frame.shape
+print 'H = ', height, ' W = ', width
 
-
-cap = cv2.VideoCapture(1)
-
-print '======================================'
-print '============= Robot-View ============='
-print '========== Daniel M. Lofaro =========='
-print '========= dan@danLofaro.com =========='
-print '======================================'
 while True:
     
     ret, frame = cap.read()
-    #img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     img = frame
-    #hight, width, depth = img.shape
-    #print "size", hight, width, depth
-
-#-----------------------------------------------------
-#--------[ Do not edit above ]------------------------
-#-----------------------------------------------------
-    # Def:
-    # ref.ref[0] = Right Wheel Velos
-    # ref.ref[1] = Left Wheel Velos
-    # tim.sim[0] = Sim Time
-    # img        = cv image in BGR format
     
-    controller.width = nx
+    # No color blub ditected = no error:
+    x = 0
+    y = 0
     
     # Convert RGB to HSV
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
     # Define upper and lower range of green color in HSV
-    lower_blue = np.array([0,100,100], dtype=np.uint8)
+    lower_blue = np.array([0,50,50], dtype=np.uint8)
     upper_blue = np.array([20,255,255], dtype=np.uint8)
 
     # Threshold the HSV image to get only green colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     
     kernel = np.ones((5,5), np.uint8)
-    erosion = cv2.erode(mask, kernel, iterations = 5)
-    dilation = cv2.dilate(erosion, kernel, iterations = 5)
+    erosion = cv2.erode(mask, kernel, iterations = 7)
+    dilation = cv2.dilate(erosion, kernel, iterations = 7)
 
     # Use findContours to get the boundry of the green blob
     contours,hierarchy = cv2.findContours(dilation,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     # Look through all the seperate contours and highlight the boundry and centroid
     for cnt in contours:
 		# Calculate moments
-        moments = cv2.moments(cnt)   
-        # maximum error:   
-        controller.error = nx                    
+        moments = cv2.moments(cnt)           
         if moments['m00']!=0:
             x = int(moments['m10']/moments['m00'])
             y = int(moments['m01']/moments['m00'])
-            print 'Center of Mass = ', '(', x, ', ', y, ')'
-            
-            # get the error from the center width:
-            controller.x = x - (nx/2)
-            controller.y = y - (ny/2)
             
             # draw contours 
             cv2.drawContours(img,[cnt],0,(0,0,255),1)   
             # draw centroids in red
-            cv2.circle(img,(x,y),10,(0,0,255),-1)      
+            cv2.circle(img,(x,y),10,(0,0,255),-1)
+            
+            # get the errors from the center:
+            x = int(x - (width/2))
+            y = int(y - (height/2))
+            break; # recognize only the first object    
 
     cv2.imshow('wctrl',img)
     
     cv2.waitKey(10)
     
+    print '\nError in x & y: ', x, '\t', y
+    
     # send the error to controller
+    controller.x = x
+    controller.y = y
     e.put(controller)
 
    
--- controller_include.py
+++ controller_include.py
@@ -1,12 +1,6 @@
-# from ctypes import *
+#!/usr/bin/env python
+
 from ctypes import Structure,c_uint16,c_double,c_ubyte,c_uint32,c_int16
-# import ach
-# import sys
-
-DEF_1 = 0
-DEF_2 = 1
-DEF_3 = 2
-
 
 CONTROLLER_REF_NAME              = 'controller-ref-chan'
 
@@ -14,8 +8,4 @@ class CONTROLLER_REF(Structure):
     _pack_ = 1
     _fields_ = [("x",    c_int16),
                 ("y",    c_int16)]
-#class HUBO_REF(Structure):
-#    _pack_ = 1
-#    _fields_ = [("ref",    c_double*HUBO_JOINT_COUNT),
-#                ("mode",   c_int16*HUBO_JOINT_COUNT),
-#                ("comply", c_ubyte*HUBO_JOINT_COUNT)]
+                
